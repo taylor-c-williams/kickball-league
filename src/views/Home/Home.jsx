3bc33441ffe9a78b68fb68802dc6d4cfdc10dc66
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-12-07 19:50:03 -0800

    trigger netlify tests

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 						src="https://i5.walmartimages.com/asr/7cd32c12-dfff-4e90-983e-9dd712f11222.019e16ba07684ed951902a359a0f5504.jpeg"
 						alt="kickball with adorable turtle printed on it"
 					/>
-					{/* another incredibly cute kickball in case I or anyone else need it:
+					{/* another incredibly cute kickball in case I or anyone else need it::
 			https://m.media-amazon.com/images/I/51vPSKT9L7L._AC_SX425_.jpg */}
 				</p>
 			</section>

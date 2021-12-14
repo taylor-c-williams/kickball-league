0d3b3448584f3382f3e55314d51bbd9ba7543bad
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-12-13 17:12:35 -0800

    removed unused var from obselete randomIng function

--- src/components/Players/RandomImg.jsx
+++ src/components/Players/RandomImg.jsx
@@ -1,5 +1,3 @@
-import { getRndInteger } from '../../services/global';
-
 export default function RandomImg({ min, max }) {
 	// const randomURL = `/kickballs/ball_${getRndInteger(min, max)}.jpg`;
 	const randomURL = `/kickballs/ball_4.jpg`;
